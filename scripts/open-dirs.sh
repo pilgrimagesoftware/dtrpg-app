21b220571042ab2b19a92c8607d0cd5b669698e8
Paul Schifferer <paul@schifferers.net>
2026-07-16 12:52:26 -0700

    chore: add utility script

---
+++ scripts/open-dirs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+set -e
+
+open "$HOME/Library/Application Support/com.pilgrimagesoftware.dtrpg"
+open "$HOME/Library/Caches/com.pilgrimagesoftware.dtrpg"
+open "$HOME/Library/Preferences/com.pilgrimagesoftware.dtrpg"
+open "$HOME/Downloads/dtrpg"
