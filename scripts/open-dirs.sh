ccab98e6c595c2b92f14553fef931b578f0960f4
Paul Schifferer <paul@schifferers.net>
2026-07-16 20:49:48 -0700

    chore: update script

--- scripts/open-dirs.sh
+++ scripts/open-dirs.sh
@@ -4,6 +4,10 @@ set -x
 set -e
 
 open "$HOME/Library/Application Support/com.pilgrimagesoftware.dtrpg"
+zed -n "$HOME/Library/Application Support/com.pilgrimagesoftware.dtrpg"
 open "$HOME/Library/Caches/com.pilgrimagesoftware.dtrpg"
+zed -n "$HOME/Library/Caches/com.pilgrimagesoftware.dtrpg"
 open "$HOME/Library/Preferences/com.pilgrimagesoftware.dtrpg"
+zed -n "$HOME/Library/Preferences/com.pilgrimagesoftware.dtrpg"
 open "$HOME/Downloads/dtrpg"
+zed -n "$HOME/Downloads/dtrpg"
